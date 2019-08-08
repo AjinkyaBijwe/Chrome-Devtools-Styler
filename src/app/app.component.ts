930c91abc389a88b08eb918b7e9daf8c4a9ab8d8
Ajinkya Bijwe <ajinkya.bijwe@gmail.com>
2019-08-07 22:33:10 -0400

    Save Font Family

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+    snackbar: MatSnackBar;
     title: string;
-    panels: any;
+    devtools: any;
+    fontFamily: any;
 
-    constructor() {
+    constructor(snackbar: MatSnackBar) {
+        this.snackbar = snackbar;
         this.title = 'Chrome Devtool Styler';
     }
 
     ngOnInit() {
-        this.panels = chrome.devtools;
-        chrome.storage.sync.get({
-            fontFamily: 'monospace',
-            externalFonts: []
-        }, items => {
-            items.externalFonts.forEach(url => {
-                fetch(url).then(resp => {
-                    return resp.text();
-                }).then(resp => {
-                    this.panels.applyStyleSheet(resp);
+        this.fontFamily = null;
+        this.loadDevtools();
+    }
+
+    loadDevtools = () => {
+        this.devtools = chrome.devtools.panels;
+        if (this.devtools) {
+            this.devtools.create('Devtool Styler', 'devtool.png', 'index.html', () => {
+                chrome.storage.sync.get(['fontFamily'], (items) => {
+                    // items.externalFonts.forEach(url => {
+                    //     fetch(url).then(resp => {
+                    //         return resp.text();
+                    //     }).then(resp => {
+                    //         this.devtools.applyStyleSheet(resp);
+                    //     });
+                    // });
+                    setTimeout(() => {
+                        this.fontFamily = items.fontFamily;
+                    }, 200);
+                    this.devtools.applyStyleSheet(this.styleTemplate(items.fontFamily));
                 });
             });
-            this.panels.applyStyleSheet(this.styleTemplate(items.fontFamily));
-        });
+        }
+    }
 
-        this.panels.create('Devtool Styler', 'index.html');
+    setChromeStorage = () => {
+        chrome.storage.sync.set({
+            fontFamily: this.fontFamily
+        }, () => {
+            this.snackbar.open('Style Stored Please Reopen Chrome Debugger', 'Dismiss' , {
+                duration: 5000
+            });
+        });
     }
 
     styleTemplate = (fontFamily) => {
@@ -41,7 +62,7 @@ export class AppComponent implements OnInit {
         :host-context(.platform-linux) .monospace, \
         :host-context(.platform-linux) .source-code, \
         .platform-linux .monospace, .platform-linux .source-code {
-            font-family: 'SF Mono' !important;
+            font-family: ${fontFamily} !important;
         }`;
     }
 }
